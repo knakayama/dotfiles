5213ccb76a266bf055dd9f4e4eacffbf1879193e
knakayama <knakayama.sh@gmail.com>
2012-03-20 15:53:59 +0900

    move utility functions to this file

--- utils.sh
+++ utils.sh
@@ -9,3 +9,29 @@ is_dotfile() {
         return 1
     fi
 }
+
+is_exist() {
+    if [ -f "$1" ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+yesno() {
+    local answer
+    echo -n "$1 (Yes/No): "
+    while read answer; do
+        case "$answer" in
+            Y|y|[Yy][Ee][Ss])
+                return 0
+            ;;
+            N|n|[Nn][Oo])
+                return 1
+            ;;
+            *)
+                echo -n "Please select Yes or No: "
+            ;;
+        esac
+    done
+}
