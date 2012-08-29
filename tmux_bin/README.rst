0c9f73d00aaf8bb3860e982208c7d79128f3e7f8
knakayama <knakayama.sh@gmail.com>
2012-08-29 09:45:12 +0900

    unko

--- tmux_bin/README.rst
+++ tmux_bin/README.rst
@@ -1,8 +1,3 @@
 scripts for tmux
 ====================
 
-::
-
-    cd dotfiles/tmux_bin
-    for i in `ls`;do sudo ln -s $PWD/$i /usr/local/bin/$i; done
-
---
+++ tmux_bin/get_wan_ip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#wan_ip=$(curl --max-time 2 -s http://whatismyip.akamai.com )
+
+#echo "$wan_ip"
+
+curl --max-time 2 -s http://whatismyip.akamai.com
+
