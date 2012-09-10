bc5c1c3946217b69c86d04040bafff257f902bc0
knakayama <knakayama.sh@gmail.com>
2012-09-10 09:30:34 +0900

    unko

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-from fabric.api import local
+import os
+
+from fabric.api import local, lcd
 
 
 def git_diff(targets):
@@ -26,8 +28,28 @@ def git_push():
     local("git push git@github.com:knakayama/dotfiles.git")
 
 
-def deploy(targets, message):
+def deploy(targets, message=None):
     git_add(targets)
     git_commit(targets, message)
     git_push()
 
+
+def create_symlinks():
+    with lcd(os.path.expanduser("~/dotfiles")):
+        exclude_files = [".gitmodules", ".gitignore", "README.rst"]
+        for f_name in os.listdir(os.getcwd()):
+            if f_name not in exclude_files:
+                os.symlink(os.path.abspath(f_name),
+                        os.path.join(os.path.expanduser("~"), f_name))
+
+
+def create_symlink():
+    pass
+
+
+def delete_symlinks():
+    pass
+
+
+def delete_symlink():
+    pass
