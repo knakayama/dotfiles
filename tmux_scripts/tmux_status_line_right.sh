0fba4478135b598f6c786967bb8abc80b09ff3ff
knakayama <knakayama.sh@gmail.com>
2013-05-22 06:53:41 +0900

    move date time to tmux left status bar

--- tmux_scripts/tmux_status_line_right.sh
+++ tmux_scripts/tmux_status_line_right.sh
@@ -2,14 +2,15 @@
 
 source ~/dotfiles/tmux_scripts/libs.sh
 
-KEYS=("uptime" "loadaverage" "mem" "date")
+#KEYS=("uptime" "loadaverage" "mem" "date")
+KEYS=("uptime" "loadaverage" "mem")
 STATUS_LINE_RIGHT=()
 
 declare -A outputs
 outputs["uptime"]=$(~/dotfiles/tmux_scripts/tmux_uptime.sh)
 outputs["loadaverage"]=$(~/dotfiles/tmux_scripts/tmux_loadaverage.sh)
 outputs["mem"]=$(~/dotfiles/tmux_scripts/tmux_mem.sh)
-outputs["date"]=$(~/dotfiles/tmux_scripts/tmux_date.sh)
+#outputs["date"]=$(~/dotfiles/tmux_scripts/tmux_date.sh)
 
 echo -n "< "
 for key in ${KEYS[@]}; do
@@ -18,9 +19,9 @@ for key in ${KEYS[@]}; do
     elif [[ "$key" == "loadaverage" ]]; then
         STATUS_LINE_RIGHT+="${FG_BG_COLOR}Avg:${outputs["$key"]}${PREFIX} < "
     elif [[ "$key" == "mem" ]]; then
-        STATUS_LINE_RIGHT+="${FG_BG_COLOR}Mem:${outputs["$key"]}${PREFIX} < "
-    else
-        STATUS_LINE_RIGHT+="${FG_BG_COLOR}${outputs["$key"]}${PREFIX}"
+        STATUS_LINE_RIGHT+="${FG_BG_COLOR}Mem:${outputs["$key"]}${PREFIX} "
+    #else
+    #    STATUS_LINE_RIGHT+="${FG_BG_COLOR}${outputs["$key"]}${PREFIX}"
     fi
 done
 
