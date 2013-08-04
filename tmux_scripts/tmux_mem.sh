c5a3a95757c18f8f0b374ba11c86e9f2b6fb45b3
knakayama <knakayama.sh@gmail.com>
2013-08-04 23:57:58 +0900

    kuso

--- tmux_scripts/tmux_mem.sh
+++ tmux_scripts/tmux_mem.sh
@@ -3,10 +3,10 @@
 PLATFORM="$(uname)"
 
 if [ "$PLATFORM" = "FreeBSD" ]; then
-    #top -b | grep 'Mem:' | sed 's/M//g' | awk 'BEGIN{OFS="M,"; ORS="M\n"} {used=$2+$4+9; print "Mem:"$6,used}'
-    vmstat -h | tail -1 | awk 'BEGIN {OFS=","} {print "Mem:" $4,$5}'
+    #vmstat -h | tail -1 | awk 'BEGIN {OFS=","} {print "Mem:" $4,$5}'
+    top -b | grep 'Mem:' | sed -e 's/G//g' -e 's/M//g' -e 's/K//g' | awk 'BEGIN{OFS="M,"; ORS="M\n"} {print "Mem:" $2+$4+$6,$8+$10}'
 elif [ "$PLATFORM" = "Linux" ]; then
     free -m | awk 'buffers/cache:' | tr -s ' ' | awk 'BEGIN {OFS="M,"; ORS="M\n"} {print "Mem:" $3,$4}'
 else
-    echo "Unkonw platform"
+    echo "Mem:Unkonw platform"
 fi
