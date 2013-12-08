d4e6e211dbff0296737b286290a169d050bbf0f7
knakayama <knakayama.sh@gmail.com>
2013-12-08 15:31:03 +0900

    I decide to use perl

--- tmux_scripts/tmux_loadaverage.sh
+++ tmux_scripts/tmux_loadaverage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #uptime | grep -Eo '([0-9]*\.[0-9]{2}\, ){2}[0-9]*\.[0-9]{2}' | sed 's/  *//g' | awk '{print "Avg:" $0}'
-uptime | perl -ne 's/\s//g; printf "Avg:%s\n",$1 if /((\d+\.\d{2},?){3})/'
+uptime | perl -ne 's/\s+//g; printf "Avg:%s\n",$1 if /average:(.*)/'
 #uptime | tr -d ' ' | perl -ne 'printf "Avg:%s\n",$1 if /((\d+\.\d{2},?){3})/'
 
