6c71a246699c86d7f4076f89d165ceb643e61efc
knakayama <knakayama.sh@gmail.com>
2013-12-08 12:40:39 +0900

    iroiro yatta yo

--- tmux_scripts/tmux_uptime.sh
+++ tmux_scripts/tmux_uptime.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-uptime | cut -d, -f1 | sed 's/^  *//g' | tr -s ' ' | cut -d' ' -f2-
+#uptime | cut -d, -f1 | sed 's/^  *//g' | tr -s ' ' | cut -d' ' -f2-
+#uptime | perl -lne 's/(\s){2,}/\1/g; print $1 if /(up.+?),/'
+uptime | tr -s ' ' | perl -lne 'print $1 if /(up.+?),/'
 
