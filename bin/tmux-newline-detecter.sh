c6e8d198a9e665b2cba89f422095478156240860
knakayama <knakayama.sh@gmail.com>
2015-04-16 21:03:29 +0900

    fix regexp

--- bin/tmux-newline-detecter.sh
+++ bin/tmux-newline-detecter.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if reattach-to-user-namespace pbpaste | hexdump | grep -qE '[[:space:]]0a|0d|0d0a[[:space:]]?'; then
+if reattach-to-user-namespace pbpaste | hexdump | grep -qE '[[:space:]](0a|0d|0d0a)[[:space:]]?'; then
     tmux confirm-before -p "Include newline. Paste?(y/n)" \
         "run-shell 'reattach-to-user-namespace pbpaste | tmux load-buffer - && tmux paste-buffer'"
 else
