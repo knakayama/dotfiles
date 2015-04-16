ef1588a13dbb553e318e5d74a1db1d0130a7ad25
knakayama <knakayama.sh@gmail.com>
2015-04-16 17:38:37 +0900

    detect newline

---
+++ bin/tmux-newline-detecter.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if reattach-to-user-namespace pbpaste | hexdump | grep -qE '[[:space:]]0a|0d|0d0a[[:space:]]?'; then
+    tmux confirm-before -p "Include newline. Paste?(y/n)" \
+        "run-shell 'reattach-to-user-namespace pbpaste | tmux load-buffer - && tmux paste-buffer'"
+else
+    reattach-to-user-namespace pbpaste | tmux load-buffer - && tmux paste-buffer
+fi
+
