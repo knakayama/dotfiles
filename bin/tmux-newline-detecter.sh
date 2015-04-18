6489e35880ae39910ed9d7c92a14ee41d12c61c8
knakayama <knakayama.sh@gmail.com>
2015-04-19 00:52:04 +0900

    use /bin/bash

--- bin/tmux-newline-detecter.sh
+++ bin/tmux-newline-detecter.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if reattach-to-user-namespace pbpaste | hexdump | grep -qE '[[:space:]](0a|0d|0d0a)[[:space:]]?'; then
     tmux confirm-before -p "Include newline. Paste?(y/n)" \
