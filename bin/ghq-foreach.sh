2a494447e1d900cd13576620df8a5c4ab04c9b98
knakayama <knakayama.sh@gmail.com>
2015-04-27 17:02:45 +0900

    add ghq-foreach.sh

---
+++ bin/ghq-foreach.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# http://qiita.com/uasi/items/610ef5745fc35745fd54
+
+ESC="$(printf "\033")"
+FG_BLUE="34"
+M="m"
+DEFAULT="[${_DEFAULT}m"
+
+usage(){
+    cat <<EOT 1>&2
+Usage: $(basename "$0") {-g <git-cmd> | -c <shell-cmd>} [-h]
+
+    -h                   print this help.
+    -g <git command>     execute git command.
+    -c <shell command>   execute shell command.
+
+EOT
+}
+
+while getopts :g:c:h opts; do
+    case "$opts" in
+        g)
+            GIT_CMD="$OPTARG"
+            ;;
+        c)
+            SHELL_CMD="$OPTARG"
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if ! which ghq >/dev/null 2>&1; then
+    echo "ghq command not found in your ${PATH}." 1>&2
+    exit 1
+fi
+
+ghq list --full-path | while read -r repo; do
+    (
+        cd "$repo"
+        printf "${ESC}[${FG_BLUE}${M}$(pwd)${ESC}${DEFAULT}\n"
+        if [[ -n "$GIT_CMD" ]]; then
+            git "$GIT_CMD"
+        else
+            $SHELL_CMD
+            :
+        fi
+    )
+done
