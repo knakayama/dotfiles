d79e3e922191513489265127487002afe39eb282
knakayama <knakayama.sh@gmail.com>
2015-06-08 20:41:28 +0900

    change footer for .zsh

--- .vim/template/template.zsh
+++ .vim/template/template.zsh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 
 # Local Variables:
-# mode: Shell-Script
-# End:
-# vim: ft=zsh
+# # mode: Shell-Script
+# # sh-indentation: 2
+# # indent-tabs-mode: nil
+# # sh-basic-offset: 2
+# # End:
+# # vim: ft=zsh sw=2 ts=2 et
