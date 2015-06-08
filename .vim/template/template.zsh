f55cecf645ad59f3f5358f9557d437f912d93957
knakayama <knakayama.sh@gmail.com>
2015-06-08 23:53:54 +0900

    fix #

--- .vim/template/template.zsh
+++ .vim/template/template.zsh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
 # Local Variables:
-# # mode: Shell-Script
-# # sh-indentation: 2
-# # indent-tabs-mode: nil
-# # sh-basic-offset: 2
-# # End:
-# # vim: ft=zsh sw=2 ts=2 et
+# mode: Shell-Script
+# sh-indentation: 2
+# indent-tabs-mode: nil
+# sh-basic-offset: 2
+# End:
+# vim: ft=zsh sw=2 ts=2 et
